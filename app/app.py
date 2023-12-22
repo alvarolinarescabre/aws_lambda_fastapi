843ca23b641ddc7c7d0e4082561829e9095cee2e
Alvaro "Chamo" Linares Cabré <alvarolinarescabre@gmail.com>
2023-12-22 17:41:45 +0100

    Refact code structure

--- app/app.py
+++ app/app.py
@@ -3,50 +3,12 @@ import uvicorn
 from fastapi import FastAPI
 from mangum import Mangum
 from conf.settings import Settings
-from fastapi.encoders import jsonable_encoder
-from fastapi.responses import JSONResponse
-from timeit import default_timer as timer
-
-from libs.helpers import delete_dir, download_file, create_dir, search_tag
-
+from routers import main
 
 settings = Settings()
 app = FastAPI(root_path=f"/{settings.api_stage}", docs_url="/docs")
-
-
-@app.get("/")
-async def index():
-    return {"hello": "world"}
-
-
-@app.get("/ping")
-async def healthcheck():
-    return {"success": "pong!"}
-
-
-@app.get("/counter")
-def counter_tags():
-    output = {}
-    start = timer()
-    create_dir(settings.dir_download)
-    create_dir(settings.dir_counted)
-
-    for key, page in enumerate(settings.random_website):
-        output[key] = search_tag(settings.dir_counted, download_file(settings.dir_download, page), "href=")
-
-    delete_dir(settings.dir_download)
-    delete_dir(settings.dir_counted)
-
-    end = timer()
-    output[10] = f"The Execution Time take: {round(end - start, 2)} seconds"
-
-    json_data = jsonable_encoder(output)
-
-    return JSONResponse(content=json_data)
-
-
+app.include_router(main.router)
 handler = Mangum(app)
-
 
 if __name__ == "__main__":
     uvicorn_app = f"{os.path.basename(__file__).removesuffix('.py')}:app"
